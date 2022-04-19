0478cb233241d4df2854ba3602e12ee398a836ac
Smooty Ziah <ehydlebeerg2@gmail.com>
2022-04-19 02:47:29 -0400

    new push

---
+++ index.js
@@ -0,0 +1,19 @@
+const myArray = [];
+
+for (var i = 2; i <= 5; i++) {
+myArray.push(i);
+}
+  
+const myArrow = [];
+var i = 2;
+
+while (i < 2) {
+    myArray.push(i);
+}
+
+
+const ourArray = [];
+
+for (var i = 13; i > 0; i-= 2){
+    ourArray.push(i);
+}
